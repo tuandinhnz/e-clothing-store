db93622807feb78767f04c63f064bc3d488d036e
Tuan  Dinh <tuandinh@Tuans-MacBook-Pro.local>
2020-09-09 11:28:28 +1200

    add items to cartdropdown component

--- src/components/CartDropDown/CartDropDown.component.js
+++ src/components/CartDropDown/CartDropDown.component.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../CartItem/CartItem.component';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({}) => {
+const CartDropdown = ({ cartItems }) => {
+  const renderedCartItems = cartItems.map((item) => <CartItem item={item} />);
   return (
     <div className="cart-dropdown">
-      <div className="cart-items"></div>
+      <div className="cart-items">{renderedCartItems}</div>
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   );
 };
-
-export default CartDropdown;
+const mapStateToProps = ({ cart: { cartItems } }) => {
+  return { cartItems };
+};
+export default connect(mapStateToProps, null)(CartDropdown);
--- src/reducers/cart.utils.js
+++ src/reducers/cart.utils.js
@@ -3,6 +3,7 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     (cartItem) => cartItem.id === cartItemToAdd.id
   );
 
+  // If item is already existed in cart, increase its quatity by one.
   if (existingCartItem) {
     return cartItems.map((cartItem) =>
       cartItem.id === cartItemToAdd.id
@@ -10,6 +11,6 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
         : cartItem
     );
   }
-
+  // If the item is not existed, add the quatity property to the item and set the base quantity to 1
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
