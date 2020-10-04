00edb3d5004652df7166a617756afa33866b07db
Tuan  Dinh <tuandinh@Tuans-MacBook-Pro.local>
2020-10-04 15:47:01 +1300

    fix spinner issue in Safari

--- client/src/components/Spinner/Spinner.styles.jsx
+++ client/src/components/Spinner/Spinner.styles.jsx
@@ -15,14 +15,12 @@ export const SpinnerContainer = styled.div`
   border: 3px solid rgba(195, 195, 195, 0.6);
   border-radius: 50%;
   border-top-color: #636767;
-  animation: spin 1s ease-in-out infinite;
-  -webkit-animation: spin 1s ease-in-out infinite;
+  animation-name: spin;
+  animation-duration: 1s;
+  animation-timing-function: ease-in-out;
+  animation-iteration-count: infinite;
+
   @keyframes spin {
-    to {
-      -webkit-transform: rotate(360deg);
-    }
-  }
-  @-webkit-keyframes spin {
     to {
       -webkit-transform: rotate(360deg);
     }
